11d6c807406b228e8f306c79691e2123a764b473
8sd <github@sebdu.de>
2019-04-26 21:33:13 +0200

    show md after parsing

--- md.js
+++ md.js
@@ -2,5 +2,6 @@ function md_parse () {
   var nodeList = document.getElementsByTagName('md');
   for (var i=0; i < nodeList.length; i++) {
     nodeList.item(i).innerHTML = marked(nodeList.item(i).firstChild.data);
+    nodeList.item(i).style.visibility = 'visible';
   }
 }
