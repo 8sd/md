13e4c03733590714e06c71db6478350f5e393ea8
8sd <github@sebdu.de>
2019-05-12 22:16:15 +0200

    Add travis yml

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 # Build one script with injected marked version
-
+echo "updating submodules…"
 git submodule update --init;
+echo "→ done"
+echo "building script"
 cat marked/marked.min.js > md.release.js;
 cat md.js >> md.release.js;
+echo "→ done"
 
 # minify script
