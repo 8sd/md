9e390a4c2fa5bad2de44ffa4570772825234b09a
8sd <github@sebdu.de>
2019-05-12 17:30:38 +0200

    Load md from src attribute

--- md.js
+++ md.js
@@ -1,7 +1,29 @@
+function getAndParseSrc (node) {
+  // load md
+  fetch(node.attributes["src"].nodeValue)
+  .then(data => {return data.text();})
+  //parse response
+  .then(response => {
+    node.innerHTML = marked(response);
+  });
+}
+
 function md_parse () {
   var nodeList = document.getElementsByTagName('md');
   for (var i=0; i < nodeList.length; i++) {
-    nodeList.item(i).innerHTML = marked(nodeList.item(i).firstChild.data);
-    nodeList.item(i).style.visibility = 'visible';
+    if(nodeList.item(i).attributes["src"] === undefined) {
+      nodeList.item(i).innerHTML = marked(nodeList.item(i).firstChild.data);
+      nodeList.item(i).style.visibility = 'visible';
+    } else {
+      (function (node) {
+        // load md
+        fetch(node.attributes["src"].nodeValue)
+        .then(data => {return data.text();})
+        //parse response
+        .then(response => {
+          node.innerHTML = marked(response);
+        });
+      })(nodeList.item(i));
+    }
   }
 }
